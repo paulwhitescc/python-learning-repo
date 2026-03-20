095ba10aadcf00fc320baf5b1a196b2f8a0711ed
Paul White <121181038+paulwhitescc@users.noreply.github.com>
2026-03-20 13:59:16 +0000

    week 4

--- intermediate/session_1/903_pipeline.py
+++ intermediate/session_1/903_pipeline.py
@@ -9,7 +9,7 @@ from sqlalchemy import (
     Table
 )
 
-from utils import clean_903_table, group_calculation, time_difference
+from utils import clean_903_table, group_calculation, time_difference, multiples_same_event, group_calculation_year,appears_on_both
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
@@ -76,7 +76,21 @@ dfs['missing']['MISSING_DURATION'] = dfs['missing'].apply(
     ,axis = 1
 )
 
-print(dfs['missing'])
+#print(dfs['missing'])
 
 #dfs["missing"]['MISSING_DURATION'] = time_difference(dfs['missing']['MIS_START_dt'],dfs['missing']['MIS_END_dt'],business_days=True)
 #print(dfs["missing"])
+
+measures['Multiple episodes'] = multiples_same_event(dfs['episodes'],event_name="Number of episodes")
+
+dfs["episodes"]["DECOM_YEAR"] = dfs["episodes"]["DECOM_dt"].dt.year
+
+print(dfs['episodes'])
+
+measures["Episodes starting per year"] = group_calculation(dfs["episodes"], "DECOM_YEAR", "Episodes starting per year")
+
+measures["Placements by year"] = group_calculation_year(dfs['episodes'], "DECOM_YEAR", "PLACE", "Placements by year")
+
+output = appears_on_both(dfs["episodes"],dfs["missing"], "CYP with episodes who have been missing")
+
+print(output)
